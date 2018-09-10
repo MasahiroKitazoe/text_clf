db6d7940543a6744564a8d5df4fad71c75343fe7
北添真広 <sas.surfer0jonny@gmail.com>
2018-09-10 14:57:21 +0900

    add scraping and text classifier

--- scraping.py
+++ scraping.py
@@ -6,35 +6,57 @@ url = input("スクレイピングしたいページURLを入力: ")
 html = urllib.request.urlopen(url)
 soup = BeautifulSoup(html, "html.parser")
 
-right_box = soup.find_all(class_="right-box")
-
 titles = []
-for element in right_box:
-  title = element.find_next('div').string
+sources = []
+
+page_name = soup.find(class_="page-name").string
+right_box = soup.find_all(class_="right-box")
+if not page_name in ['イノベーション', 'ジョブオファー']:
+  for element in right_box:
+    title = element.find_next('div').string
+    titles.append(title)
+  for element in right_box:
+    source = element.find_next(class_='meta').find_next('div').string
+    sources.append(source)
+
+news_cards = soup.find_all(class_="news-card")
+for element in news_cards:
+  title_link = element.find_next('a')
+  title = title_link.find_next(class_="title").string
   titles.append(title)
 
-sources = []
-for element in right_box:
-  source = element.find_next(class_='meta').find_next('div').string
-  sources.append(source)
+publisher_containers = soup.find_all(class_="publisher-container")
+if publisher_containers:
+  for element in publisher_containers:
+    source = element.find_next('span').string
+    sources.append(source)
+else:
+  for element in soup.find_all(class_="sponsored-container"):
+    source = element.find_next(class_="sponsor-name").string
+    sources.append(source)
 
-page_names = []
-page_name = soup.find(class_="page-name").string
+classes = {'ビジネス': 0, 'キャリア・教育': 1, '金融・マーケット': 2, 'イノベーション': 3, 'ジョブオファー': 4, '政治・経済': 5, '社会・スポーツ': 6, 'テクノロジー': 7}
+
+i = 0
+indexes = []
+themes = []
 for _ in range(len(titles)):
-  page_names.append(page_name)
+  themes.append(classes[page_name])
+  i += 1
+  indexes.append(i)
 
-df = pd.DataFrame({'title': titles,
+df = pd.DataFrame({'index': indexes,
+                   'title': titles,
                    'source': sources,
-                   'theme': page_names})
+                   'theme': themes})
+df = df.set_index('index')
 
 file_names = {'テクノロジー': 'technology', 'ビジネス': 'business', '政治・経済': 'politics_economics', '金融・マーケット': 'finance_market', 'キャリア・教育': 'carrier_education', '社会・スポーツ': 'society_sports', 'イノベーション': 'innovation', 'ジョブオファー': 'others'}
 
 csv_name = './datasets/{}.csv'.format(file_names[page_name])
-
-df_origin = pd.read_csv(csv_name)
+df_origin = pd.read_csv(csv_name, index_col=0)
 
 df_new = pd.concat([df_origin, df])
-
+df_new = df_new.drop_duplicates(['title', 'source'])
+df_new = df_new.reset_index(drop=True)
 df_new.to_csv(csv_name, mode="w")
-
-# TODO: インデックスの処理を何とかしないと、実行するたびに謎の列が追加されてしまうので、要対処
--- text_clf.py
+++ text_clf.py
@@ -4,4 +4,81 @@ from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
 import requests
+import MeCab
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 
+
+class WordDividor:
+    INDEX_CATEGORY = 0
+    INDEX_ROOT_FORM = 6
+    TARGET_CATEGORIES = ["名詞", " 動詞",  "形容詞", "副詞", "連体詞", "感動詞"]
+
+    def __init__(self, dictionary="mecabrc"):
+        self.dictionary = dictionary
+        self.tagger = MeCab.Tagger(self.dictionary)
+
+    def extract_words(self, text):
+        if not text:
+            return []
+
+        words = []
+
+        node = self.tagger.parseToNode(text)
+        while node:
+            features = node.feature.split(',')
+
+            if features[self.INDEX_CATEGORY] in self.TARGET_CATEGORIES:
+                if features[self.INDEX_ROOT_FORM] == "*":
+                    words.append(node.surface)
+                else:
+                    # prefer root form
+                    words.append(features[self.INDEX_ROOT_FORM])
+
+            node = node.next
+
+        return words
+
+if __name__ == '__main__':
+
+  df_business = pd.read_csv('./datasets/business.csv', index_col=0)
+  df_carrier_edu = pd.read_csv('./datasets/carrier_education.csv', index_col=0)
+  df_finance_market = pd.read_csv('./datasets/finance_market.csv', index_col=0)
+  df_politics_economics = pd.read_csv('./datasets/politics_economics.csv', index_col=0)
+  df_society_sports = pd.read_csv('./datasets/society_sports.csv', index_col=0)
+  df_technology = pd.read_csv('./datasets/technology.csv', index_col=0)
+  df_innovation = pd.read_csv('./datasets/innovation.csv', index_col=0)
+  df_others = pd.read_csv('./datasets/others.csv', index_col=0)
+
+  df = pd.concat([df_business, df_carrier_edu, df_finance_market, df_innovation, df_others, df_politics_economics, df_society_sports, df_technology])
+
+  df = df.reset_index(drop=True)
+
+  df = df.drop_duplicates(['title', 'source'])
+
+  df['data'] = df['title'] + ' ' + df['source']
+
+  text_data = df['data']
+  text_target = df['theme']
+
+  X_train, X_test, y_train, y_test = train_test_split(text_data, text_target, test_size=0.20, random_state=0)
+
+  wd = WordDividor()
+  cv = CountVectorizer(min_df=2, analyzer=wd.extract_words)
+
+  vect = cv.fit(X_train)
+  X_train = vect.transform(X_train)
+
+  param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+  grid = GridSearchCV(LogisticRegression(), param_grid, cv=5)
+  grid.fit(X_train, y_train)
+  print(grid.best_score_)
+  print(grid.best_params_)
+
+  X_test = vect.transform(X_test)
+  print(grid.score(X_test, y_test))
+
+  sample = ['無所属・小川淳也氏、立憲会派入り表明', '大坂なおみの優勝にブーイング　２０歳の新女王が涙の謝罪「こんな終わり方ですみません」']
+  sample = vect.transform(sample)
+  print(grid.predict(sample))
