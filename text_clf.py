ad944d3c743df1ced71784717fdd68555108842d
北添真広 <sas.surfer0jonny@gmail.com>
2018-09-11 14:19:18 +0900

    modify installed library

--- text_clf.py
+++ text_clf.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
-import MeCab
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
