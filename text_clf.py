634857b6289d25b93617533550fab601136bb643
北添真広 <sas.surfer0jonny@gmail.com>
2018-09-11 13:49:45 +0900

    debug character codes

--- text_clf.py
+++ text_clf.py
@@ -1,8 +1,9 @@
+# coding:utf-8
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
-from natto import MeCab
+import MeCab
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
